3fc1efa69b57007bfcaa68f01bca77269269f954
Zhou Maoen <mezhou887@foxmail.com>
2017-07-23 21:14:37 +0800

    去掉原有的 代码

--- src/main/java/com/mezhou887/crawler/dao/UrlMapper.java
+++
@@ -1,7 +0,0 @@
-package com.mezhou887.crawler.dao;
-
-import com.mezhou887.system.core.Mapper;
-import com.mezhou887.crawler.model.Url;
-
-public interface UrlMapper extends Mapper<Url> {
-}
--- src/main/java/com/mezhou887/crawler/dao/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.mezhou887.crawler.dao;
-
-import com.mezhou887.crawler.model.User;
-import com.mezhou887.system.core.Mapper;
-
-public interface UserMapper extends Mapper<User> {
-}
